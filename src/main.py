9949fd39a774a4c8cf7837321344efe7f9106315
Zen <user@Zens-MacBook-Air.local>
2025-08-05 17:48:15 +0800

    textnode and texttype

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("hello world")
+from textnode import *
+
+
+def main():
+    testnode = TextNode("the quick brown fox", TextType.BOLD)
+    print(testnode)
+    print("hello world")
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+class TextType(Enum):
+    PLAIN = "plain"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode:
+
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, node):
+        if not isinstance(node, TextNode):
+            return False
+        
+        return(node.text == self.text and
+            node.text_type == self.text_type and
+            node.url == self.url)
+
+    def __repr__(self):
+        return(f"TextNode({self.text}, {self.text_type.value}, {self.url})")
